b792acd96ceb3ef72dde230fb95a3d4158b1e496
kurniawan2805 <kurniawan2805@gmail.com>
2022-09-26 17:45:28 +0700

    modify index.js

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
       </main>
 
